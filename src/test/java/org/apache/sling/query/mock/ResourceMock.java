6cf0b135af20b5b0448c5478f5b5225c52644126
Cris Rockwell <cmrockwe@umich.edu>
2020-07-23 11:49:58 -0400

    SLING-9600 Updated commons-lang to commons-lang3

--- src/test/java/org/apache/sling/query/mock/ResourceMock.java
+++ src/test/java/org/apache/sling/query/mock/ResourceMock.java
@@ -26,7 +26,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceMetadata;
 import org.apache.sling.api.resource.ResourceResolver;
